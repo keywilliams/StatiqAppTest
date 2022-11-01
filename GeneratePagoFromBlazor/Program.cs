a3867e16aa8578a388a4ad4122be0739fcc3daf8
Key Williams <keywilliams@outlook.com>
2022-11-01 19:13:20 +0000

    KWSS - Add TMC.Frontend.Microsite

--- GeneratePagoFromBlazor/Program.cs
+++ GeneratePagoFromBlazor/Program.cs
@@ -18,32 +18,5 @@ namespace GeneratePagoFromBlazor
                                      .GenerateSiteByLanguage()
                                      .RunAsync();
         }
-
-        //private static void AddPipelines(Bootstrapper bootstrapper)
-        //{
-        //    var tmcService = new TmcService();
-        //    var language = tmcService.GetIndex();
-
-        //    foreach (var index in language.IndexCollection)
-        //    {
-        //        bootstrapper.BuildPipeline($"Render Index in {index.Key}", builder => builder
-        //            .WithInputReadFiles("Index.cshtml")
-        //            .WithProcessModules(new RenderRazor().WithModel(Config.FromValue(index.Value)))
-        //            .WithOutputWriteFiles(new NormalizedPath($"{index.Key}/Index.html")));
-
-        //        bootstrapper.BuildPipeline($"Render Privacy in {index.Key}", builder => builder
-        //            .WithInputReadFiles("Privacy.cshtml")
-        //            .WithProcessModules(new RenderRazor().WithModel(Config.FromValue(index.Value.Privacy)))
-        //            .WithOutputWriteFiles(new NormalizedPath($"{index.Key}/Privacy.html")));
-
-        //        foreach (var item in index.Value.Items)
-        //        {
-        //            bootstrapper.BuildPipeline($"Render IndexItem {item.Name} in {index.Key}", builder => builder
-        //                .WithInputReadFiles("IndexItem.cshtml")
-        //                .WithProcessModules(new RenderRazor().WithModel(Config.FromValue(item)))
-        //                .WithOutputWriteFiles(new NormalizedPath($"{index.Key}/{item.Name}.html")));
-        //        }
-        //    }
-        //}
     }
 }
