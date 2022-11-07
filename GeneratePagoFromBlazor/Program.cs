2c252219712ce8fe06dfa515414e782f318acfc4
Key Williams <keywilliams@outlook.com>
2022-11-07 19:26:01 +0000

    KWSS - Add TMC website.

--- GeneratePagoFromBlazor/Program.cs
+++ GeneratePagoFromBlazor/Program.cs
@@ -1,22 +1,36 @@
 ﻿using GeneratePagoFromBlazor.Pipelines;
+using GeneratePagoFromBlazor.Utils;
 
 namespace GeneratePagoFromBlazor
 {
     class Program
     {
-        public static async Task<int> Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            return await Bootstrapper.Factory
+            //await Bootstrapper.Factory
+            //                         .CreateDefault(args)
+            //                         .AddPipeline<JavaScriptPipeline>()
+            //                         .AddPipeline<CssScriptPipeline>()
+            //                         .AddPipeline<ResourcesPipeline>()
+            //                         .AddPipeline<ScssScriptPipeline>()
+            //                         //.DownloadInputFiles(new Uri("https://github.com/keywilliams/StatiqAppTest/raw/master/input.zip"))
+            //                         //.GenerateScssFiles()
+            //                         //.GenerateSiteByLanguage()
+            //                         .AddWeb()
+            //                         .RunAsync();
+
+            await Bootstrapper.Factory
                                      .CreateDefault(args)
                                      .AddPipeline<JavaScriptPipeline>()
                                      .AddPipeline<CssScriptPipeline>()
                                      .AddPipeline<ResourcesPipeline>()
                                      .AddPipeline<ScssScriptPipeline>()
-                                     //.DownloadInputFiles(new Uri("https://github.com/keywilliams/StatiqAppTest/raw/master/input.zip"))
+                                     .DownloadInputFiles(new Uri("https://github.com/keywilliams/StatiqAppTest/raw/master/Input2.zip"))
                                      //.GenerateScssFiles()
                                      //.GenerateSiteByLanguage()
                                      .AddWeb()
                                      .RunAsync();
+            return;
         }
     }
 }
