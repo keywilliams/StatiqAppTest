e234dc4e7fa954f7c2d3c63d862fd6204e69964b
Key Williams <keywilliams@outlook.com>
2022-11-14 21:46:29 +0000

    KWSS - Update Layout to be more SEO friendly.

--- GeneratePagoFromBlazor/Models/LayoutModel.cs
+++ GeneratePagoFromBlazor/Models/LayoutModel.cs
@@ -5,6 +5,8 @@
         public string Name { get; set; }
         public string Title { get; set; }
         public string MetaDescription { get; set; }
+        public string OgTitle { get; set; }
+        public string Robots { get; set; }
         public List<Language> SiteLanguages { get; set; }
 
 
