5aaffb6f0540d9bced2d3f2f0e063530d3ba2b97
Key Williams <keywilliams@outlook.com>
2022-12-08 17:01:26 +0000

    KWSS - Update input

--- GeneratePagoFromBlazor/Models/BaseModel.cs
+++ GeneratePagoFromBlazor/Models/BaseModel.cs
@@ -4,9 +4,10 @@
     {
         public string Description { get; set; }
         public ContentType ContentType { get; set; }
-        public decimal ImdbRating { get; set; }
+        public string ImdbRating { get; set; }
         public int Year { get; set; }
         public string Image { get; set; }
         public string BannerImage { get; set; }
+        public string DefaultTrailerUrl { get; set; }
     }
 }
