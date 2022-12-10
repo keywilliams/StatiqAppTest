a8eea834d2a4ad5d0f041075d0f9506d8edd9838
Key Williams <keywilliams@outlook.com>
2022-12-10 12:50:34 +0000

    KWSS - Update input.

--- GeneratePagoFromBlazor/Models/LayoutModel.cs
+++ GeneratePagoFromBlazor/Models/LayoutModel.cs
@@ -3,14 +3,14 @@
     public class LayoutModel
     {
         public string Name { get; set; }
+        public string HtmlName { get; set; }
         public string Title { get; set; }
         public string MetaDescription { get; set; }
         public string OgTitle { get; set; }
         public string Robots { get; set; }
-        public List<Language> SiteLanguages { get; set; }
-
-        public bool MainPage { get; set; } = false;
         public string Logo { get; set; }
+        public List<Language> SiteLanguages { get; set; }
+        public bool MainPage { get; set; } = false;
         public string CompanyName { get; set; }
     }
 
