3eca401baf141a576cf0c7a4627f5b256e7993fb
Key Williams <keywilliams@outlook.com>
2022-12-22 18:23:59 +0000

    KWSS - Add episode data.

--- GeneratePagoFromBlazor/Models/TvDetailModel.cs
+++ GeneratePagoFromBlazor/Models/TvDetailModel.cs
@@ -20,7 +20,11 @@
     public class Episode
     {
         public int Id { get; set; }
+        public string Name { get; set; }
         public string Description { get; set; }
         public string Duration { get; set; }
+        public string Position { get; set; }
+        public string Image { get; set; }
+        public int Year { get; set; }
     }
 }
